3a3825da26a02f954c456adee27e9538a9a672cc
iamzin <eunzin.park@gmail.com>
2022-02-20 14:56:27 +0900

    chore: initial

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # inter-office
+
+interface와 office의 합성어로 공유 오피스 이용 고객분들 간의 interface 역할을 하는 서비스 입니다.
+미팅룸 예약, 커뮤니티, 회사별 그룹 채팅 등의 기능을 제공합니다.
---
+++ src/main/java/com/interoffice/InterofficeApplication.java
@@ -0,0 +1,13 @@
+package com.interoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterofficeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InterofficeApplication.class, args);
+  }
+
+}
